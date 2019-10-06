1ea8e48b3c281d285da9938208640d30116a6855
Mohamed Zaki <dev@id3m.net>
2019-10-06 11:39:24 +0200

    add data

--- name-contests/database/test-pg-data.sql
+++ name-contests/database/test-pg-data.sql
@@ -73,3 +73,16 @@ VALUES
 (3,FALSE,2),
 (4,TRUE,1),
 (4,TRUE,2);
+
+    "name": "Test1",
+    "type": "Dog",
+    "age": 5,
+    "clientMutationId": "abcdef"
+mutation AddNewContest($input: ContestInput!) {
+  AddContest(input: $input) {
+    "name": "Test1",
+    "type": "Dog",
+    "age": 5,
+    "clientMutationId": "abcdef"
+  }
+} 
